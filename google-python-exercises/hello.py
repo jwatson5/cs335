e582aa50bddccca613d185cbea860c15d8a5701e
Watson <jswatson@gmail.com>
2019-01-15 16:38:08 +0000

    modified hello.py

--- google-python-exercises/hello.py
+++ google-python-exercises/hello.py
@@ -25,8 +25,8 @@ def main():
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
-    name = 'World'
-  print 'Hello', name
+    name = 'World Watson'
+  print 'Hello', name, 'Watson'
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
