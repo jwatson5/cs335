745c8196e8dd3615d3ae960c178944f93f50dc9d
Watson <jswatson@gmail.com>
2019-01-17 16:46:25 +0000

    daily 1/17/19

---
+++ multiply.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def mult(x, y):
+    print(str(x * y))
+
+if __name__ == '__main__':
+  num1 = int(sys.argv[1])
+  num2 = int(sys.argv[2])
+  mult(num1, num2)
